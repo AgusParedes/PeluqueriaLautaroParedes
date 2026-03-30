fa1b5e6a65abf1f27eb0f7d25a924f1ca1462349
Agustin Andres Paredes <paredesagustin15@gmail.com>
2026-03-30 11:31:22 -0300

    Finished Home

--- Peluqueria-Front/src/components/Home/Home.jsx
+++ Peluqueria-Front/src/components/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import Foto_Logo from '../../../public/Foto_Logo.png'
 import './Home.scss'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div className='home__container'>
-      <img src={Foto_Logo} alt="Logo" />
-      <button className='home__button'>Reservar Turno</button>
+      <div className='home__logo'>
+        <img src="/Foto_Logo.png" alt="Logo" />
+      </div>
+      <Link to="/BookingPage">
+        <button className='home__button'>Reservar Turno</button>
+      </Link>
     </div>
   )
 }
